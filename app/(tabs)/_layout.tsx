61b9bf6fbb01dd89bf31746fae7c73781f04fc52
leejongil1046-dev <leejongil1046.dev@gmail.com>
2026-03-15 22:21:50 +0900

    feat: 네비게이션바 아이콘 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,51 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { HapticTab } from "@/components/haptic-tab";
+import { Colors } from "@/constants/theme";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors["light"].tint,
+        tabBarInactiveTintColor: Colors["light"].tabIconDefault,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+        tabBarStyle: {
+          height: 120,
+          paddingTop: 10,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "캘린더",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="calendar-alt" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="todoList"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "할 일 목록",
+          tabBarIcon: ({ color }) => (
+            <Feather name="check-square" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="user"
+        options={{
+          title: "사용자",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user-circle" size={24} color={color} />
+          ),
         }}
       />
     </Tabs>
