f3672932ac91c35c750b7156057957711e50f9fa
leejongil1046-dev <leejongil1046.dev@gmail.com>
2026-03-18 13:41:14 +0900

    feat: 할 일 입력 박스에 슬라이드 기능 추가

--- components/todo-input-form.tsx
+++ components/todo-input-form.tsx
@@ -1,6 +1,6 @@
 import { Image } from "expo-image";
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useRef, useState } from "react";
+import { Animated, Pressable, StyleSheet, View } from "react-native";
 
 type TodoInputFormProps = {
   label: string;
@@ -9,20 +9,45 @@ type TodoInputFormProps = {
 };
 
 export function TodoInputForm() {
-  const [done, setDone] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+  const heightAnim = useRef(new Animated.Value(50)).current;
+  const toggleExpand = () => {
+    const toValue = expanded ? 50 : 200;
+    Animated.timing(heightAnim, {
+      toValue,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+    setExpanded((prev) => !prev);
+  };
 
   return (
-    <View style={styles.todoInputCard}>
-      <Image
-        source={require("@/assets/images/plus.svg")}
-        style={{ width: 30, height: 30 }}
-        contentFit="contain"
-      />
-    </View>
+    <Pressable onPress={toggleExpand}>
+      <Animated.View
+        style={[styles.todoInputCardWrapper, { height: heightAnim }]}
+      >
+        <View style={styles.todoInputCard}>
+          <Image
+            source={require("@/assets/images/plus.svg")}
+            style={{ width: 25, height: 25 }}
+            contentFit="contain"
+          />
+        </View>
+        {/* expanded === true일 때 아래에 입력 폼 추가 예정 */}
+      </Animated.View>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
+  todoInputCardWrapper: {
+    width: "100%",
+    height: 50,
+    borderRadius: 12,
+    backgroundColor: "#F4F4F5",
+    marginVertical: 6,
+    justifyContent: "flex-start",
+  },
   todoInputCard: {
     width: "100%",
     height: 50,
@@ -30,17 +55,7 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 20,
     backgroundColor: "#F4F4F5",
-    marginVertical: 6,
     justifyContent: "center",
     alignItems: "center",
-  },
-  todoText: {
-    fontSize: 13,
-    fontWeight: "500",
-    paddingBottom: 2,
-  },
-  todoTextDone: {
-    textDecorationLine: "line-through",
-    color: "#6B7280",
   },
 });
